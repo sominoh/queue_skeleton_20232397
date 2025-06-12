0f85207ea129757d38fc975ca6518c89fbf79b91
오소민 <145252436+sominoh@users.noreply.github.com>
2025-06-12 22:42:11 +0900

    thread-safe를 위한 mutex 추가

--- qtype.h
+++ qtype.h
@@ -1,9 +1,9 @@
-#ifndef _QTYPE_H  // header guard
+#ifndef _QTYPE_H
 #define _QTYPE_H
 
-// ==========이 파일은 수정 가능==========
+#include <mutex> // mutex 추가
 
-typedef unsigned int Key;  // 값이 클수록 높은 우선순위
+typedef unsigned int Key;  
 typedef void* Value;
 
 typedef struct {
@@ -13,22 +13,18 @@ typedef struct {
 } Item;
 
 typedef struct {
-    bool success;   // true: 성공, false: 실패
+    bool success;   
     Item item;
-    // 필드 추가 가능
 } Reply;
 
 typedef struct node_t {
     Item item;
     struct node_t* next;
-    // 필드 추가 가능
 } Node;
 
 typedef struct {
-    Node* head, tail;
-    // 필드 추가 가능
+    Node* head;
+    std::mutex lock; // thread-safe를 위한 뮤텍스 추가
 } Queue;
-
-// 이후 자유롭게 추가/수정: 새로운 자료형 정의 등
 
 #endif
